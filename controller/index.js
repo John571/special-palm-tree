1dcbcee610807044facc4bee6b54322605e5fa16
Sergei <sergmaloi@gmail.com>
2021-09-06 18:41:53 +0300

    rename

---
+++ controller/index.js
@@ -0,0 +1,68 @@
+import dotenv from "dotenv";
+import amqp from "amqplib";
+import express from "express";
+import crypto from "crypto";
+// crypto.randomBytes(4).toString('hex')
+
+dotenv.config();
+let channel = null;
+let connection = null;
+let q = null;
+const QUEUE = "shopping";
+
+let connect = async () => {
+  try {
+    connection = await amqp.connect(process.env.RABBIT_LOCAL_CON_STR);
+    channel = await connection.createChannel();
+    await channel.assertQueue(QUEUE);
+    console.log("[V] Connected to queue.");
+  } catch (error) {
+    console.error(error);
+    console.log("[X] Connection to queue failed.");
+  }
+};
+
+await connect();
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Hello from shopingApp Controller"));
+
+app.post("/msg", async (req, res) => {
+  q = await channel.assertQueue("", { exclusive: true });
+  let msg = {
+    content: req.body.msg,
+    type: "job_request",
+  };
+  let id = crypto.randomBytes(4).toString("hex");
+  console.log(
+    `controller - sending msg ${msg.content} of type ${msg.type} with id ${id}, waiting for processing...`
+  );
+
+  channel.sendToQueue(QUEUE, Buffer.from(JSON.stringify(msg)), {
+    correlationId: id,
+    replyTo: q.queue,
+  });
+
+  channel.consume(q.queue, (data) => {
+    if (data && data.properties.correlationId == id) {
+      let ms = JSON.parse(data.content);
+      let idd = data.properties.correlationId;
+      console.log(
+        `controller received answer ${ms.content} of type ${ms.type} with id ${idd}`
+      );
+      channel.ack(data);
+      channel.deleteQueue(q.queue);
+    }
+  });
+  res.send("Ok!");
+});
+
+app.listen(4000, () =>
+  console.log("[*]Shopping Controller is listening on port 4000")
+);
+
+process.on("beforeExit", () => {
+  console.log("[X] Stopping the controller");
+  connection.close();
+});
