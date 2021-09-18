414d00ebdc8a8a97f7f2ed556c26ee84c2167158
Yohana <67158525+John571@users.noreply.github.com>
2021-09-18 15:30:34 +0300

    Shopping List Image Service
    *)The service receives from the UI "Three" main parameters :
    
    1) Group ID.
    2) Product ID.
    3) Message. // Meanwhile this parameter gets a string, which needs to be replaced to get an image //
    4) Date and Time. // Updated automatically //
    
    *) Meanwhile the Service is locally connected to Mongo DB, which inserts and retrieves data "To/From" the database . // GET, POST //

---
+++ ImageService/ImageService/Dtos/ImageDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageService.Dtos
+{
+    public record ImageDto
+    {
+        public int Id { get; init; }
+        public int ProudctId { get; init; }
+        public string Message { get; init; }
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ ImageService/ImageService/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using ImageService.Dtos;
+using ImageService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageService
+{
+    public static class Extensions
+    {
+       public static ImageDto AsDto(this Image image)
+       {
+            return new ImageDto
+            {
+                Id = image.Id,
+                ProudctId =image.ProudctId,
+                Message = image.Message,
+                CreateDate = image.CreateDate
+            };
+       }
+    }
+}
