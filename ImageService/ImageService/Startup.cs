f57268389a2cb6480017041b2d6cda4862b88d31
Yohana Farraj <Yohana.farraj@gmail.com>
2021-09-27 16:56:53 +0300

    Updated Image Service

--- ImageService/ImageService/Startup.cs
+++ ImageService/ImageService/Startup.cs
@@ -35,10 +35,6 @@ namespace ImageService
 
             services.AddSingleton<IImagesRepository, MongoDbImagesRepository>();
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ImageService", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,8 +43,6 @@ namespace ImageService
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ImageService v1"));
             }
 
             app.UseHttpsRedirection();
