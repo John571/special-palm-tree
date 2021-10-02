5d16df99397452fed8d1ad4212345620a0328923
Yohana Farraj <Yohana.farraj@gmail.com>
2021-10-02 14:46:17 +0300

    Service permissions denied

--- ImageServic-UsingBlob/ImageService/Blob Storage Uploader/BlobStorageURL.cs
+++ ImageServic-UsingBlob/ImageService/Blob Storage Uploader/BlobStorageURL.cs
@@ -11,8 +11,8 @@ namespace ImageService
 {
     public class BlobStorageURL
     {
-        private static string connectionString = "DefaultEndpointsProtocol=https;AccountName=imageuploader2106;AccountKey=R8ncsFI92nMNljF9GFR2PEkDs8EILlOFB1Kvzel1ErmEFg4TfI/f/LnXchLgRZe7HaQLx4yLP4z80ra3vQqoQg==;EndpointSuffix=core.windows.net";
-        private static string containerName = "imageblobstoragecontainer";
+        private static string connectionString = "<Your Blob Storage Connection String>";
+        private static string containerName = "<Your Blob Storage Container Name>";
         public static string Upload(IFormFile photo)
         {
             BlobContainerClient container = new BlobContainerClient(connectionString, containerName);
